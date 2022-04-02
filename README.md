713bb4e38261af135d4484547cfbb3cc9bc1d851
张志阵 <1809642355@qq.com>
2022-04-02 17:20:15 +0800

    feat:cms管理油猴插件初版

---
+++ README.md
@@ -0,0 +1,32 @@
+<!--
+ * @Description: File Description
+ * @FilePath: /study/temperMonkey/backend_cms_mangement/README.md
+ * @LastEditors: zzz
+ * @LastEditTime: 2022-04-02 17:06:14
+-->
+### **`temperMonkey脚本`** backend的cms管理 
+
+
+> 该脚本旨在用快快捷初始化cms页面并自动同步配置
+
+<!-- ![backend的cms管理](./images/backend.png) -->
+
+### 使用方法
+
+#### 初始化cms页面
+
+> 一键开启cms管理页面，省去每次刷新页面要手动点开cms管理页面的繁琐操作
+
+* 初始化cms页面
+* 初始化cms模块
+
+#### 同步配置到页面
+
+> 一键同步cms配置到cms页面，省去每次都要更新过配置之后手动更新页面的繁琐操作
+
+* 在cms模块页面修改过脚本之后，点击「同步配置到页面」即可等待该脚本执行同步动作
+* 当然，最关键的一步确认是需要你手动确认的，以免误触
+
+### 入口
+<!-- * [Greasy Fork](https://greasyfork.org/zh-CN/scripts/442300-splunk-search)
+* [Gist](https://gist.github.com/zzailianlian/0466db69a7cb663aea32a467c7be8f34) -->
---
+++ index.js
@@ -0,0 +1,85 @@
+/*
+ * @Description: File Description
+ * @FilePath: /study/temperMonkey/backend_cms_mangement/index.js
+ * @LastEditors: zzz
+ * @LastEditTime: 2022-04-02 16:59:46
+ */
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://backend.meitun-test.com/index
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=meitun-test.com
+// @grant        GM_setValue
+// @grant        GM_getValue
+// ==/UserScript==
+
+(function () {
+  'use strict';
+  const wrapperToolsContainer = $('<div id="wrapperToolsContainer"></div>')
+  wrapperToolsContainer.css({
+    position: 'fixed',
+    top: '200px',
+    right: 200,
+    padding: '12px',
+    background: '#3498db',
+    borderRadius: '4px',
+    fontSize: '14px',
+    zIndex: 999,
+    opacity: 0.1,
+  });
+  wrapperToolsContainer.hover(function () { $(this).css({ opacity: 1 }); }, function () { $(this).css({ opacity: .1 }); })
+  const initCMSModule = $('<div>初始化cms页面</div>')
+  initCMSModule.css({
+    padding: '12px',
+    color: 'white',
+    background: '#2ecc71',
+    borderRadius: '4px',
+    fontSize: '14px',
+    border: '1px solid #27ae60',
+    zIndex: 999,
+    marginBottom: '24px',
+  });
+
+  const syncConfig = $('<div>同步配置到页面</div>')
+  syncConfig.css({
+    padding: '12px',
+    color: 'white',
+    background: '#2ecc71',
+    borderRadius: '4px',
+    fontSize: '14px',
+    border: '1px solid #27ae60',
+    zIndex: 999,
+
+  });
+
+  $('body').append(wrapperToolsContainer)
+  wrapperToolsContainer.append(initCMSModule).append(syncConfig)
+
+  const CMS_PAGE = 'CMS_PAGE'
+  initCMSModule.on('click', () => {
+    // 初始化cms管理tab
+    $("a:contains('v2.0CMS页面管理')").click()
+    // GM_setValue(CMS_PAGE, $('#menubar_tabs').find('h3').last().find('a').attr('id'));
+    console.log('---',$("a:contains('v2.0CMS页面管理')").attr('id'))
+    $("a:contains('v2.0CMS系统模板')").click()
+  })
+
+  syncConfig.on('click', () => {
+    // 同步配置模板到页面
+    $("input[value='保      存']").click()
+    $('#menubar_tabs').find('a:contains("v2.0CMS页面管理")').click()
+    $(document.getElementById('mainIframe_tabli_780').contentWindow.document.body).find('tr').find('td:contains("783")').parent().find('button:contains("编辑")').click()
+    setTimeout(function () {
+      $(document.getElementById('mainIframe_modifyPage783').contentWindow.document.body).find('input[value="下一步"]').last().click()
+      setTimeout(function () {
+        $(document.getElementById('mainIframe_modifyPage783').contentWindow.document.body).find('input[value="保存"]').last().click()
+        setTimeout(function () {
+          $(document.getElementById('mainIframe_tabli_780').contentWindow.document.body).find('tr').find('td:contains("783")').parent().find('button:contains("同步线上")').click()
+        }, 2000)
+      }, 2000);
+    }, 2000)
+  })
+})();
